c06b32ad54c31b60bb3828be8514b2fe621a962b
Oksana <oksanavasilevna77@gmail.com>
2024-12-29 02:24:59 +0200

    add search

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,3 +1,23 @@
-export default function SearchBar() {
-  return <div>SearchBar</div>;
+import s from "./SearchBar.module.css";
+import { IoIosSearch } from "react-icons/io";
+
+export default function SearchBar({ onSubmit }) {
+  return (
+    <div>
+      <form onSubmit={onSubmit} className={s.searchForm}>
+        <input
+          name="query"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          className={s.input}
+        />
+        
+        <button className={s.button} type="submit">
+          <IoIosSearch size={24} className={s.icon}/>
+        </button>
+      </form>
+    </div>
+  );
 }
