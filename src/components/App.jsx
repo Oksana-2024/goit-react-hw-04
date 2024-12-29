33eba788a78a120d5cb39e9117c2777d78c2a1dd
Oksana <oksanavasilevna77@gmail.com>
2024-12-29 02:27:36 +0200

    add  and settings components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,99 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./Header/Header";
+import { fetchImages } from "../services/api";
+import SearchBar from "./SearchBar/SearchBar";
 import Loader from "./Loader/Loader";
+import toast, { Toaster } from "react-hot-toast";
+import ImageModal from "./ImageModal/ImageModal";
+import ImageGallery from "./ImageGallery/ImageGallery";
+import ErrorMessage from "./ErrorMessage/ErrorMessage";
+import LoadMoreBtn from "./LoadMoreBtn/LoadMoreBtn";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [messageError, setMessageError] = useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState();
+  const [loadMore, setLoadMore] = useState(false);
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const query = form.query.value;
+
+    if (query.trim().length > 0) {
+      setSearch(query);
+      fetchImagesData(query);
+      form.reset();
+      return;
+    }
+    toast.error("Please enter search data.");
+  };
+
+  const fetchImagesData = async (query, page = 1) => {
+    try {
+      setLoader(true);
+      setMessageError(false);
+      const { results, total_pages } = await fetchImages("/search/photos", {
+        query: query,
+        page: page,
+      });
+
+      page === 1 ? setImages(results) : setImages(images.concat(...results));
+
+      setLoadMore(total_pages > page);
+      results.length < 1 && toast.error("Not found any matches");
+    } catch (err) {
+      setMessageError(true);
+      console.error(err);
+    } finally {
+      setLoader(false);
+    }
+  };
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const handleClick = (e, item) => {
+    e.preventDefault();
+    openModal();
+    setSelectedPhoto(item);
+  };
+
+  const onLoadMore = () => {
+    const nextPage = page + 1;
+    setPage(nextPage);
+    fetchImagesData(search, nextPage);
+  };
+
   return (
     <>
-      <Header />
-      <Loader />
+      <Header>
+        <SearchBar onSubmit={handleSubmit} />
+      </Header>
+      <main>
+        <Loader visible={loader} />
+        <ImageGallery images={images} onClick={handleClick} />
+        <Toaster position="top-right" />
+        <ImageModal
+          closeModal={closeModal}
+          modalIsOpen={modalIsOpen}
+          openModal={openModal}
+          alt={selectedPhoto?.alt_description}
+          image={selectedPhoto?.urls.regular}
+        />
+        <ErrorMessage messageError={messageError} />
+        {loadMore && <LoadMoreBtn onClick={onLoadMore} />}
+      </main>
     </>
   );
 }
