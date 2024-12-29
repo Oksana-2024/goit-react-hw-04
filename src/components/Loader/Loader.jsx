6da3b57a090603459a69a9578f19bce05d10a09c
Oksana <oksanavasilevna77@gmail.com>
2024-12-29 04:20:10 +0200

    add styles for loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,20 @@
 import { RotatingLines } from "react-loader-spinner";
+import s from "./Loader.module.css";
 
-export default function Loader({visible}) {
+export default function Loader({ visible }) {
   return (
-    <RotatingLines
-      visible={visible}
-      height="96"
-      width="96"
-      color="grey"
-      strokeWidth="5"
-      animationDuration="0.75"
-      ariaLabel="rotating-lines-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-    />
+    <div className={s.loader}>
+      <RotatingLines
+        visible={visible}
+        height="96"
+        width="96"
+        color="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        ariaLabel="rotating-lines-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
   );
 }
