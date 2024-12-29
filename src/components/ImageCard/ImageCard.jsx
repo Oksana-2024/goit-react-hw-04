5740ea845ab69e22c60811488fcf44fc307df263
Oksana <oksanavasilevna77@gmail.com>
2024-12-29 02:24:18 +0200

    add card image

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,9 +1,36 @@
+import s from "./ImageCard.module.css";
+import { FcLike } from "react-icons/fc";
 
-
-export default function ImageCard() {
+export default function ImageCard({
+  onClick,
+  urls,
+  likes,
+  alt_description,
+  user,
+}) {
   return (
-    <div>
-  <img src="" alt="" />
-</div>
-  )
+    <>
+      <a
+        className={s.linkItem}
+        href={urls.regular}
+        onClick={(e) => onClick(e, { urls, alt_description })}
+      >
+        <img
+          src={urls.small}
+          alt={alt_description}
+          width={300}
+          className={s.imageCard}
+        />
+        <div className={s.discrBox}>
+          <div className={s.author}>
+            <p>Author: <span className={s.userName}>{user.name.toLowerCase()}</span></p>
+          </div>
+          <div className={s.likes}>
+            <FcLike />
+            <p>{likes}</p>
+          </div>
+        </div>
+      </a>
+    </>
+  );
 }
